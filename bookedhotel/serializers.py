49ce8c464fa32f2fe22f1e39e171d23efabca4af
shifatx62 <shifatx62@gmail.com>
2024-04-29 09:48:39 -0700

    addbook

--- bookedhotel/serializers.py
+++ bookedhotel/serializers.py
@@ -3,9 +3,6 @@ from . import models
 
 
 class BookedSerializer(serializers.ModelSerializer):
-    buyer = serializers.StringRelatedField(many=False)
-    hotel = serializers.StringRelatedField(many=False)
-
     class Meta:
         model = models.hotel_booked
         fields = "__all__"
