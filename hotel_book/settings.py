dcf70d2c98aa6d94145f4de9e8e8baa6e37763f3
shifatx62 <shifatx62@gmail.com>
2024-04-29 18:06:48 -0700

    githeader

--- hotel_book/settings.py
+++ hotel_book/settings.py
@@ -137,7 +137,4 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = env("email")
 EMAIL_HOST_PASSWORD = env("pass_word")
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",  # Allow your development server
-    "https://hotel-book-v78k.onrender.com",  # Allow your production domain
-]
+CORS_ALLOWED_ORIGINS = ["*"]
